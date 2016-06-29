77f932fe2875dabad16d25c9758568b9532b4b03
sammy <fbispk@gmail.com>
2016-06-29 21:33:29 +0800

    remove user from groupFriends

--- src/containers/AddDebt.js
+++ src/containers/AddDebt.js
@@ -2,7 +2,7 @@ import { connect } from 'react-redux';
 import DebtList from '../components/AddDebt';
 
 const mapStateToProps = (state) => ({
-  groupFriends: state.groupFriends.list,
+  groupFriends: state.groupFriends.list.filter((x) => (x !== state.login.username)),
   debtList: state.debtList.list,
   repayList: state.debtList.repayList,
   newDebt: state.newDebt,
