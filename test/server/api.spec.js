884db3595481f912d0c75e70c8d9666361e8b2b7
darrenyaoyao <b02901042@ntu.edu.tw>
2016-07-21 18:54:48 +0800

    addfriend himself test

--- test/server/api.spec.js
+++ test/server/api.spec.js
@@ -6,14 +6,36 @@ describe('Server API unit test', () => {
   const p = new Promise((resolve) => setTimeout(() => resolve(request(app)), 5000));
   it('should successfully login with correct account and password', (done) => {
     p.then((res) =>
-        res.post('/api/login')
-          .send({
-            username: 'admin',
-            password: 'admin',
-          })
-          .expect(200))
+      res.post('/api/login')
+        .send({
+          username: 'admin',
+          password: 'admin',
+        })
+        .expect(200)
+      )
       .then((res) => {
-        expect(res.body.success).to.be.true; })
+        expect(res.body.success).to.be.true;
+      })
+      .then(() => {
+        done();
+      })
+      .catch((err) => {
+        console.log('in error', err);
+        done(err);
+      });
+  }).timeout(10000);
+  it('should return err 3 for add user himself to be a friend', (done) => {
+    p.then((res) =>
+      res.post('/api/addfriend')
+        .send({
+          friendname: 'admin',
+          username: 'admin',
+        })
+        .expect(200)
+      )
+      .then((res) => {
+        expect(res.body.err).to.equal(3);
+      })
       .then(() => {
         done();
       })
